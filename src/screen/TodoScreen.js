f91306244dde976de5d4b0aa837138ad12ca9dff
gabbygood <gabriellegoylan@gmail.com>
2024-09-09 09:11:04 +0800

    ADD SCREEN

---
+++ src/screen/TodoScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, TextInput, View } from 'react-native'
+
+
+const TodoScreen = () => {
+  return (
+    <View>
+      <Text>TodoScreen</Text>
+    </View>
+  )
+}
+
+export default TodoScreen
+
+const styles = StyleSheet.create({})
