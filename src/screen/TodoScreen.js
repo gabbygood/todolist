227b9fb30441c20b46f71c0e03ada2946e8b74b5
gabbygood <gabriellegoylan@gmail.com>
2024-09-11 21:47:12 +0800

    update again

--- src/screen/TodoScreen.js
+++ src/screen/TodoScreen.js
@@ -1,7 +1,8 @@
 import { FlatList, StyleSheet, Text, TextInput, Touchable, TouchableOpacity, View } from 'react-native'
-import React from 'react';
+import React, { useState } from 'react';
+import { useFonts } from 'expo-font';
 import { IconButton } from 'react-native-paper'
-const dummyData = [
+const tempData = [
   {
     id: "01",
     title: "Wash Car"
@@ -13,8 +14,13 @@ const dummyData = [
 ]
 
 
+
 const TodoScreen = () => {
+
   const [number, onChangeNumber] = React.useState('');
+  const [todo, setTodo] = useState("")
+  const [editedTodo, setEditedTodo] = useState(null)
+
   const renderTodos = ({ item, index }) => {
     return (
       <View style={{
@@ -27,24 +33,50 @@ const TodoScreen = () => {
         flexDirection: "row",
         alignItems: "center",
       }}>
-        <Text style={{ fontSize: 20, color: "white", fontWeight: 800 }}>{item.title}</Text>
-        <IconButton icon="pencil" />
-        <IconButton icon="trash-can" />
+        <Text style={{ fontSize: 20, color: "white", fontWeight: 800, flex: 1 }}>{item.title}</Text>
+        <IconButton icon="pencil" iconColor='#8cf9ff' onPress={() => handleEditTodo(item)} />
+        <IconButton icon="trash-can" iconColor='red' onPress={() => handleDeleteTodo(item.id)} />
       </View>
     )
   }
+  const [todoList, setTodoList] = useState([])
+
+  const handleAddTodo = () => {
+    setTodoList([...todoList, { id: Date.now().toString(), title: todo }])
+    setTodo("")
+  }
+  const handleDeleteTodo = (id) => {
+    const updateTodoList = todoList.filter((todo) => { todoList.id !== id })
+
+    setTodoList(updateTodoList)
+  }
+  const handleEditTodo = (todo) => {
+    setEditedTodo(todo)
+    setTodo(todo.title)
+  }
+  const handleUpdateTodo = () => {
+    const updatedTodos = todoList.map((item) => {
+      if (item.id === editedTodo.id) {
+        return { ...item, title: todo }
+      }
+      return item
+    })
+    setTodoList(updatedTodos)
+    setEditedTodo(null)
+    setTodo("")
+  }
   return (
     <View style={{ marginHorizontal: 16 }}>
-      <Text style={{ marginTop: 50, marginLeft: 140 }}>TodoScreen</Text>
+      <Text style={styles.todoApp}>Todo App</Text>
       <TextInput
         style={styles.input}
-        onChangeText={onChangeNumber}
-        value={number}
+        onChangeText={(userText) => setTodo(userText)}
+        value={todo}
         placeholder="Add Task"
         keyboardType="default"
       />
 
-      <TouchableOpacity style={{
+      {editedTodo ? <TouchableOpacity onPress={() => handleUpdateTodo()} style={{
         backgroundColor: "#000",
         borderRadius: 6,
         paddingVertical: 8,
@@ -52,10 +84,22 @@ const TodoScreen = () => {
         alignItems: "center"
       }}>
         <Text style={styles.text_one}>
-          Add Text
+          Update Task
         </Text>
-      </TouchableOpacity>
-      <FlatList data={dummyData} renderItem={renderTodos} />
+      </TouchableOpacity> :
+        (<TouchableOpacity onPress={() => handleAddTodo()} style={{
+          backgroundColor: "#000",
+          borderRadius: 6,
+          paddingVertical: 8,
+          marginVertical: 34,
+          alignItems: "center"
+        }}>
+          <Text style={styles.text_one}>
+            Add Task
+          </Text>
+        </TouchableOpacity>)
+      }
+      <FlatList data={todoList} renderItem={renderTodos} />
     </View>
   )
 }
@@ -73,6 +117,14 @@ const styles = StyleSheet.create({
     margin: 12,
     borderWidth: 1,
     padding: 10,
-    fontSize: 20
+    fontSize: 20,
+    paddingVertical: 8,
+    width: 345
   },
+  todoApp: {
+    fontFamily: "Playwrite-CU",
+    marginTop: 50,
+    marginLeft: 100,
+    fontSize: 40
+  }
 })
